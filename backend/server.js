a08ecc3e99bfc049b65fc1699edc7ebf71f266b6
Utsha Basak <134736097+UtshaBasak@users.noreply.github.com>
2025-09-05 19:22:47 +0600

    Update CORS origins in server.js

--- backend/server.js
+++ backend/server.js
@@ -48,7 +48,8 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://127.0.0.1:5173',
   'http://localhost:3000',
-  'http://127.0.0.1:3000'
+  'http://127.0.0.1:3000',
+  'https://urbannest-f2tp.onrender.com'
 ].filter(Boolean);
 
 app.use(cors({
